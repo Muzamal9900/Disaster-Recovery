cc8caade3b2d31573a5ec9b44c6761dda7d14d87
Rana Muzamil <mmlrana00@gmail.com>
2026-03-18 08:05:52 +0500

    update package json and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import bundleAnalyzer from '@next/bundle-analyzer'
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -91,4 +85,4 @@ const nextConfig = {
   },
 }
 
-export default withBundleAnalyzer(nextConfig)
+export default nextConfig
